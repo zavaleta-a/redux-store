82173d74151622c763c77a826cfabdac4f336232
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2022-01-10 21:12:18 -0600

    deploying attempt hotel

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "../client/images")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static("client/build"));
 }
 
 // app.get('*', (req, res) => {
